f48c4c77c8bc60d204458a414d3f1e9478444c39
jordy25519 <beauchjord@gmail.com>
2025-11-17 16:46:53 +0800

    add legacy_layout macro to help rewrite zero-copy structs with u/i128 fields

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::{quote, format_ident};
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use quote::{format_ident, quote};
+use syn::{Data, DeriveInput, Fields, Ident, ItemStruct, Type, parse_macro_input, parse_quote};
 
 #[allow(clippy::panic)]
 #[proc_macro_attribute]
@@ -55,4 +55,99 @@ pub fn assert_no_slop(_: TokenStream, input: TokenStream) -> TokenStream {
         #expanded
     };
     output.into()
+}
+
+/// #[legacy_layout]
+///
+/// Mark a struct as using the legacy u128/i128 layout for stable zero-copy
+/// deserialization across compiler versions.
+///
+/// Usage:
+///
+///     #[legacy_layout]                  // rewrite fields + generate accessors
+///     #[legacy_layout(no_accessors)]    // rewrite fields only
+#[proc_macro_attribute]
+pub fn legacy_layout(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut s = parse_macro_input!(item as ItemStruct);
+
+    // ---- Parse attr flags ----
+    let attr_args = parse_macro_input!(attr as syn::AttributeArgs);
+    let mut disable_accessors = false;
+
+    for arg in attr_args {
+        match arg {
+            syn::NestedMeta::Meta(syn::Meta::Path(path)) if path.is_ident("no_accessors") => {
+                disable_accessors = true;
+            }
+            _ => {
+                return syn::Error::new_spanned(arg, "Unknown attribute argument")
+                    .to_compile_error()
+                    .into();
+            }
+        }
+    }
+
+    // ---- Rewrite fields ----
+    let mut rewritten_fields: Vec<(Ident, bool)> = Vec::new();
+
+    for field in s.fields.iter_mut() {
+        let Some(ident) = &field.ident else { continue };
+
+        if let Type::Path(type_path) = &field.ty {
+            if let Some(seg) = type_path.path.segments.last() {
+                match seg.ident.to_string().as_str() {
+                    "u128" => {
+                        field.ty = parse_quote!(crate::math::bn::compat::u128);
+                        rewritten_fields.push((ident.clone(), false));
+                    }
+                    "i128" => {
+                        field.ty = parse_quote!(crate::math::bn::compat::i128);
+                        rewritten_fields.push((ident.clone(), true));
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    let struct_ident = &s.ident;
+    let (impl_generics, ty_generics, where_clause) = s.generics.split_for_impl();
+
+    // ---- Generate getters + setters unless disabled ----
+    let impl_block = if rewritten_fields.is_empty() || disable_accessors {
+        quote! {}
+    } else {
+        let accessors = rewritten_fields.iter().map(|(ident, is_signed)| {
+            let getter_name = ident.clone();
+            let setter_name = Ident::new(&format!("set_{}", ident), ident.span());
+
+            let ret_ty = if *is_signed {
+                quote!(i128)
+            } else {
+                quote!(u128)
+            };
+
+            quote! {
+                pub fn #getter_name(&self) -> #ret_ty {
+                    self.#ident.into()
+                }
+
+                pub fn #setter_name(&mut self, value: #ret_ty) {
+                    self.#ident = value.into();
+                }
+            }
+        });
+
+        quote! {
+            impl #impl_generics #struct_ident #ty_generics #where_clause {
+                #(#accessors)*
+            }
+        }
+    };
+
+    // ---- Final expanded output ----
+    TokenStream::from(quote! {
+        #s
+        #impl_block
+    })
 }
